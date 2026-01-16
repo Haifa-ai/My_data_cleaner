1f78cf16e3c9af0b713aaca89da6b04b9e3874f6
Haifa-ai <me44.com@gmail.com>
2026-01-16 08:41:58 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 from youtube_transcript_api import YouTubeTranscriptApi
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
